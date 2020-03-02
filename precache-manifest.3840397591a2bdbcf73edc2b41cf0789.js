d94033f7f99bb3067ec823381fb400716a8b71fa
aldairmh98 <aldairmh98@outlook.es>
2020-03-01 21:59:47 -0600

    Updates

---
+++ precache-manifest.3840397591a2bdbcf73edc2b41cf0789.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "490f4869076d3ded7fe80d86a97c5183",
+    "url": "/momingo/static/media/will-smith.490f4869.jpg"
+  },
+  {
+    "revision": "6d8eb209aabb3ad27e71a87977e198b8",
+    "url": "/momingo/static/media/milos.6d8eb209.jpg"
+  },
+  {
+    "revision": "538a753b4a667562fb622b073642e157",
+    "url": "/momingo/static/media/gorrito.538a753b.png"
+  },
+  {
+    "revision": "209804ac6a12e38197a5",
+    "url": "/momingo/static/js/runtime~main.209804ac.js"
+  },
+  {
+    "revision": "dccb470dea7241abb090",
+    "url": "/momingo/static/js/main.dccb470d.chunk.js"
+  },
+  {
+    "revision": "c5379051239404edc4b8",
+    "url": "/momingo/static/js/2.c5379051.chunk.js"
+  },
+  {
+    "revision": "dccb470dea7241abb090",
+    "url": "/momingo/static/css/main.8d9ba0fd.chunk.css"
+  },
+  {
+    "revision": "e620029df34d4d9d9afefa85520aaa62",
+    "url": "/momingo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/momingo/precache-manifest.3840397591a2bdbcf73edc2b41cf0789.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/momingo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
